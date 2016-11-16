e88a41d8e7d4c31a857fcb5f832fb1f8a0b7fcc9
Raghu Betina <raghu@firstdraft.com>
2016-11-16 11:54:16 +0000

    Direct association between users and saved_resumes

--- app/models/saved_resume.rb
+++ app/models/saved_resume.rb
@@ -1,6 +1,8 @@
 class SavedResume < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :saved_resumes,
+             :dependent => :destroy
+
   has_many   :organizations,
              :dependent => :destroy
 
