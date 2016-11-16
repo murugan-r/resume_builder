0ad3246180c58ac8501910a665a4ee3c917fbd7c
Raghu Betina <raghu@firstdraft.com>
2016-11-16 11:54:16 +0000

    Direct association between purpose_statements and sr_purpose_statements

--- app/models/purpose_statement.rb
+++ app/models/purpose_statement.rb
@@ -1,6 +1,10 @@
 class PurposeStatement < ApplicationRecord
   # Direct associations
 
+  has_many   :sr_purpose_statements,
+             :foreign_key => "purpose_statements_id",
+             :dependent => :destroy
+
   belongs_to :user
 
   # Indirect associations
--- app/models/sr_purpose_statement.rb
+++ app/models/sr_purpose_statement.rb
@@ -1,6 +1,8 @@
 class SrPurposeStatement < ApplicationRecord
   # Direct associations
 
+  belongs_to :purpose_statements
+
   # Indirect associations
 
   # Validations
