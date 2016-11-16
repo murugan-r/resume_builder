b91280a2282555d2a8a21c5132a2c94ad23f7c4e
Raghu Betina <raghu@firstdraft.com>
2016-11-16 11:55:01 +0000

    Whitelist columns in SrPurposeStatement for ActiveAdmin

--- app/admin/sr_purpose_statement.rb
+++ app/admin/sr_purpose_statement.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register SrPurposeStatement do
 
+ permit_params :purpose_statements_id, :resume_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
