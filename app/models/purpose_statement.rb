0e203ac337b59549df2879ff82252fb458cddd20
Raghu Betina <raghu@firstdraft.com>
2016-11-16 11:54:16 +0000

    Direct association between users and purpose_statements

--- app/models/purpose_statement.rb
+++ app/models/purpose_statement.rb
@@ -1,6 +1,8 @@
 class PurposeStatement < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :purpose_statements,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
