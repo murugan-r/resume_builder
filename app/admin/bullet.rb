d5045c982e900da3c05239e3f30f1a341f7a61a6
Raghu Betina <raghu@firstdraft.com>
2016-11-16 11:54:32 +0000

    Whitelist columns in Bullet for ActiveAdmin

--- app/admin/bullet.rb
+++ app/admin/bullet.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Bullet do
 
+ permit_params :organization_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
