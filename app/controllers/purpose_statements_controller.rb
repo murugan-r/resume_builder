d439df8e304aaa2cd6e68743150b4fed745904dc
Raghu Betina <raghu@firstdraft.com>
2016-11-16 11:55:04 +0000

    Only purpose_statement user can modify purpose_statement

--- app/controllers/purpose_statements_controller.rb
+++ app/controllers/purpose_statements_controller.rb
@@ -1,4 +1,14 @@
 class PurposeStatementsController < ApplicationController
+  before_action :current_user_must_be_purpose_statement_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_purpose_statement_user
+    purpose_statement = PurposeStatement.find(params[:id])
+
+    unless current_user == purpose_statement.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @purpose_statements = PurposeStatement.all
 
